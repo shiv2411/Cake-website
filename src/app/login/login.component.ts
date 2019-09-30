4f790fd224b60f1b0d975de982ebbf2c281f8e60
kajol <kajolkumarisingh222@gmail.com>
2019-10-01 00:50:18 +0530

    login+Signup

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { LoginData } from './login.model';
 
 @Component({
   templateUrl: './login.component.html',
@@ -9,7 +10,13 @@ import { NgForm } from '@angular/forms';
 export class LoginComponent {
 
   LoginForm(form: NgForm) {
-    
+    const data: LoginData = {
+      firstname: form.value.firstname,
+      lastname: form.value.lastname,
+      email: form.value.email,
+      password: form.value.password,
+      cpassword: form.value.confirmpassword
+    };
   }
 
 }
